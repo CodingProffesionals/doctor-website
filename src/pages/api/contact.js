113065dc11eace1576b9165644f299b6830fbb75
Udit <easyprograming1994@gmail.com>
2024-07-28 10:21:54 +0530

    changed status code

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -7,8 +7,19 @@ export default function handler(req, res) {
         user: 'yudit1994@gmail.com',
         pass: 'wdtozclshfmzjzhx',
       },
-      secure: false,
+      secure: true,
     }) 
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        res.send(200).json({ message: "Error" });
+
+      } else {
+        console.log("Server is ready to take our messages");
+        res.status(200).json({ message: "Server is ready to take our messages" });
+
+      }
+    });
     const mailData = {
       from: req.body.email,
       to: 'yudit1994@gmail.com',
@@ -17,13 +28,12 @@ export default function handler(req, res) {
       html: `<div>${req.body.message}</div></br><b>Contact Details</b></br><p>Name: ${req.body.first + ' '+req.body.last}</p><p>Phone no: ${req.body.phone}</p><p>Sent from:
       ${req.body.email}</p>`
     }
-    transporter.sendMail(mailData, function (err, info) {
-      if(err)
-        console.log(err)
-      else
-        console.log(info)
-    })
+    // transporter.sendMail(mailData, function (err, info) {
+    //   if(err)
+    //     console.log(err)
+    //   else
+    //     console.log(info)
+    // })
       console.log(req.body);
-      res.send(200);
     }
     
